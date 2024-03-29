d16d9915cc933ea0e89fd0f74176ff5898151066
FaSTick-By <bobeerkurwa@gmail.com>
2024-03-29 17:59:38 +0500

    added project files

---
+++ database/models.py
@@ -0,0 +1,43 @@
+from database.db import connect
+
+
+class DBTableCreator:
+    def __init__(self):
+        self.connection, self.cursor = connect('../todos.db')
+
+    def create_users_table(self):
+        sql = """
+            DROP TABLE IF EXISTS users;
+            CREATE TABLE IF NOT EXISTS users(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT UNIQUE
+            );
+        """
+        self.cursor.executescript(sql)
+        self.connection.commit()
+# todos
+# title_unique
+# description
+# user_id
+# created_at
+# is_finished boolean
+    def create_todos_table(self):
+        sql = """
+        DROP TABLE IF EXISTS users;
+            CREATE TABLE IF NOT EXISTS users(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEXT UNIQUE,
+                description TEXT,
+                user_id INTEGER REFERENCES users(id),
+                created_at TEXT,
+                is_finished BOOLEAN
+            );
+        """
+        self.cursor.executescript(sql)
+        self.connection.commit()
+
+
+creator = DBTableCreator()
+creator.create_users_table()
+creator.create_todos_table()
+creator.connection.close()
